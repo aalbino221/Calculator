ec27695491817c5955b412c0c6a2b82ef67280b0
Andre <aalbino221@gmail.com>
2022-01-06 18:35:26 -0300

    Keyboard - Redesign

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ const funcArray = Object.keys(funções)
 const body = document.body
 const resultado = document.querySelector('.result')
 const operations = document.querySelector('.operations')
-body.addEventListener('keydown' ,  (e)=> {
-    console.log(e.key)}) 
 
 const botão = document.querySelectorAll('button') ;
 
@@ -27,7 +25,7 @@ let a = 0 ; let b = 0 ; let func;
     if(false == isNaN(Number(key.textContent))) {
             (resultado.textContent == a) ? (resultado.textContent = '' , resultado.textContent += key.textContent) : resultado.textContent += key.textContent  }
 
-    else if (key.classList == '.') {resultado.textContent += key.textContent }
+    else if (key.classList == '.') {(Number(resultado.textContent) % 1 == 0)? resultado.textContent += key.textContent : null }
 
     else if (key.classList == 'ac') {resultado.textContent = '' ; a = 0}
 
@@ -45,5 +43,32 @@ let a = 0 ; let b = 0 ; let func;
 
     else if (key.classList == 'igual') {b = Number(resultado.textContent.slice(1));
         resultado.textContent = func(a,b) , a = func(a,b)}})}
+
+body.addEventListener('keydown', (e)=> {
+
+    if(false == isNaN(Number(e.key))) {
+            (resultado.textContent == a) ? (resultado.textContent = '' , resultado.textContent += e.key) : resultado.textContent += e.key  }
+        
+    else if (e.key == '.') {(Number(resultado.textContent) % 1 == 0)? resultado.textContent += e.key : null}
+        
+    else if (e.key =='delete') {resultado.textContent = '' ; a = 0}
+        
+    else if (e.key == 'backspace') {c()}
+        
+    else if (e.key == '+') {a = Number(resultado.textContent) ;func = funções.add; resultado.textContent = '+';}
+        
+    else if (e.key == '-') {a = Number(resultado.textContent) ;func = funções.menos; resultado.textContent = '-';}
+        
+    else if (e.key == '*' || e.key == 'x') {a = Number(resultado.textContent) ;func = funções.multi; resultado.textContent = 'x';}
+        
+    else if (e.key == '/') {a = Number(resultado.textContent) ;func = funções.div; resultado.textContent = '/';}
+        
+    else if (e.key == '%') {a = Number(resultado.textContent) ;func = funções.remain; resultado.textContent = '%';}
+        
+    else if (e.key == '=') {b = Number(resultado.textContent.slice(1));
+            resultado.textContent = func(a,b) , a = func(a,b)}
+                
+    else if (e.key == 'Enter') {b = Number(resultado.textContent.slice(1));
+        resultado.textContent = func(a,b) , a = func(a,b)}})
 
 
